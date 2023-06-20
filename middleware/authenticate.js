bbda92b95d16248be80730f6d1f7ca62f3f8f323
Pranjali <cpranjali02@gmail.com>
2023-06-20 22:27:59 +0530

    changes

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -4,7 +4,8 @@ const User=require("../model/userSchema");
 
 const Authenticate= async(req,res,next)=>{
   try{
-      const token=req.cookies.jwtoken;
+    const token = req.headers?.authorization?.split(" ")[1];
+    console.log(token)
       const verifyToken=await jwt.verify(token, process.env.SECRET_KEY);
       const rootUser=await User.findById(verifyToken._id)
       if (!rootUser){throw new Error('User not Found')}
